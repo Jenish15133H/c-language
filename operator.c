21908eb472f4b112ecbb0e12bb706c80b92d0438
jaykalathiya123 <kalathiyajay020@gmail.com>
2024-11-29 12:16:54 +0530

    operator

---
+++ operator.c
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+ main()
+
+{
+
+// int a,b, ans1;
+
+// printf("enter the value of a and b:");
+// scanf("%d%d",&a,&b);
+
+// ans1=a*a + 2*a*b +b*b ;
+
+// printf("answer is :%d" , ans1);
+
+// //2
+
+// int x,y, ans2;
+
+// printf("\n\n\n\nenter the value of x and y:");
+// scanf("%d%d",&x,&y);
+
+// ans2= x*x - 2*x*y + y*y ;
+
+// printf("answer is :%d" , ans2);
+
+// //3
+
+// int d,e,ans3;
+
+// printf("\n\n\n\nenter the value of d and e :");
+// scanf("%d%d",&d,&e);
+
+// ans3= d*d*d + 3*e*d*d + 3*e*e*d + e*e*e ;
+
+
+// printf("\n\nanswer is :%d" , ans3);
+// //4
+
+// int a,b,c ;
+
+// a=15 ;
+// b=20 ;
+
+// c = a ;
+// a=b ;
+// b=c ;
+
+// printf("\n\n\nthe value of a and b is :%d\t%d" ,a , c);
+
+
+
+
+//without using third vaiable
+
+int x,y ;
+
+x =15; 
+
+y=20;
+
+x = x+y ;
+y = x-y ;
+x = x-y ;
+
+
+
+
+printf("the value of x and y is :%d\t%d ",x ,y);
+
+
+
+
+
+
+
+}
