6cc53d26891df6e720940ec8ecabffe508abe484
jaykalathiya123 <kalathiyajay020@gmail.com>
2024-12-05 13:08:09 +0530

    pr2

--- pr2.c
+++ pr2.c
@@ -2,24 +2,43 @@
 main()
 {
 
-    int a;
-    char grade;
+    int a, grade;
+    grade = 0;
     printf("enter your score :");
     scanf("%d", &a);
-   
-    (a <= 100 && a >= 90) ? printf("your grade is A") :
 
-    (a <= 89 && a >= 80)   ? printf("your grade is B")
-    : (a <= 79 && a >= 70) ? printf("your grade is C")
-    : (a <= 69 && a >= 60) ? printf("your grade is D")
-                           : printf("you are passed");
+    (a <= 100 && a >= 90) ? printf("your grade is A") : grade++;
+
+    (a <= 89 && a >= 80) ? printf("your grade is B") : grade++;
+    (a <= 79 && a >= 70) ? printf("your grade is C") : grade++;
+    (a <= 69 && a >= 60) ? printf("your grade is D") : printf(".");
 
     // 2
-    switch ()
+    switch (grade)
     {
 
-    case :
-        printf("Excellent work!");
+    case 0:
+        printf(",Excellent work!");
         break;
+    case 1:
+        printf(",Well done");
+        break;
+    case 2:
+        printf(",good job");
+        break;
+
+    case 3:
+        printf(",you are not passed");
+        break;
+
+        // // 3
+        // if (grade <= 3)
+        // {
+        //     printf(",You are eligible for the next level.");
+        // }
+        // else
+        // {
+        //     printf("you are not eligible for the next level");
+        // }
     }
 }
