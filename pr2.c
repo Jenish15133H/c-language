31ec30d1ca0413ad94a37cbc32796ebc7adc9d3b
jaykalathiya123 <kalathiyajay020@gmail.com>
2024-12-06 11:55:51 +0530

    simple_grade_cal

--- pr2.c
+++ pr2.c
@@ -3,40 +3,46 @@ main()
 {
 
     int a;
+    char grade;
     printf("enter your score :");
     scanf("%d", &a);
-    char grade ;
 
-     grade = a>90 
+    grade = (a >= 90) ? 'A' : (a >= 80) ? 'B'
+                          : (a >= 70)   ? 'C'
+                          : (a >= 60)   ? 'D'
+                                        : 'E';
 
+    printf("your grade is %c\n", grade);
 
-
-     
     switch (grade)
     {
 
-    case 0:
+    case 'A':
         printf(",Excellent work!");
         break;
-    case 1:
+    case 'B':
         printf(",Well done");
         break;
-    case 2:
+    case 'C':
         printf(",good job");
         break;
 
-    case 3:
-        printf(",you are not passed");
+    case 'D':
+        printf(",you could do better");
         break;
 
-        // // 3
-        if (grade <= 3)
-        {
-            printf(",You are eligible for the next level.");
-        }
-        else
-        {
-            printf("you are not eligible for the next level");
-        }
+    case 'E':
+        printf(",you are not passed");
+        break;
+    }
+
+    // 3
+    if (grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D')
+    {
+        printf(",You are eligible for the next level.");
+    }
+    else
+    {
+        printf("you are not eligible for the next level");
     }
 }
