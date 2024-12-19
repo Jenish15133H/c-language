6f7b769e664d4f9d35a1a44b3d09360530244378
Jenish15133H <129548527+Jenish15133H@users.noreply.github.com>
2024-12-19 10:58:05 +0530

    Update pr4.c

--- pr4.c
+++ pr4.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    
-    int i,j ,  k;
-    
-    //1
-    for(i=41 ; i<=45 ; i++)
-    
+
+    int i, j, k;
+
+    // 1
+    for (i = 41; i <= 45; i++)
+
     {
-        for(j=41 ; j<=i ; j++)  
+        for (j = 41; j <= i; j++)
         {
-           printf("%d",j)  ;  
-         }
-           printf("\n");
-    } 
-   
-    //2
-    //3
-      for(i=5 ; i>=1 ; i--){
-      
-      for (k=i; k>1;k--)
-      {
-          printf(" ");
-      }
-      
-      
-    
-    
-        for(j=i ; j<=5 ; j++)  
-        {
-           printf("%d",j)  ;  
-         }
-           printf("\n");
-    } 
-    
-    //4
-    
-    for(i=5;i>=1;i--)
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    // 2
+    // 3
+    for (i = 5; i >= 1; i--)
     {
-        for (j=1; j<=i;j++)
+
+        for (k = i; k > 1; k--)
         {
-            if(j%2!=0)
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    // 4
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (j % 2 != 0)
             {
                 printf("1 ");
             }
@@ -49,53 +47,30 @@ int main ()
             {
                 printf("0 ");
             }
-            
-            
         }
-        printf("\n")
-        ;
+        printf("\n");
     }
-    
-    
-    //5
-    
-       for(i=5 ; i>=1 ; i--)
-  {
-      
-      for (k=i; k>1;k--)
-      {
-          printf(" ");
-      }
-        for(j=i ; j<=5 ; j++)  
-        {
-           printf("%d",j)  ;  
-         }
-           
-           
-             for(k=4;k>=i;k--)
+
+    // 5
+
+    for (i = 5; i >= 1; i--)
     {
-        printf("%d",k);
+
+        for (k = i; k > 1; k--)
+        {
+            printf(" ");
+        }
+        for (j = i; j <= 5; j++)
+        {
+            printf("%d", j);
+        }
+
+        for (k = 4; k >= i; k--)
+        {
+            printf("%d", k);
+        }
+        printf("\n");
     }
-    printf("\n");
-    }
-    
-  
- 
-  
-
-
-    
-
-       
-          
-             
-                
-                   
-                      
-                         
-                            
-                                  
 }
-
 
 
